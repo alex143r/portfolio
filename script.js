726da2e541e8c374e1a70b7e5ace199341cebda6
Alexander <alex143r@stud.kea.dk>
2020-10-23 01:03:12 +0200

    staging init

--- script.js
+++ script.js
@@ -29,3 +29,20 @@ window.addEventListener("scroll", function (e) {
     document.querySelector(".show_scroll").style.opacity = "1";
   }
 });
+
+window.addEventListener("scroll", hideNav);
+function hideNav() {
+  console.log("yo");
+  document.querySelector("nav").style.opacity = 0.5;
+  //document.querySelector("nav").style.top = "-40px";
+
+  let currScroll = window.pageYOffset;
+
+  setTimeout(showNav, 200);
+  function showNav() {
+    if (currScroll === window.pageYOffset) {
+      document.querySelector("nav").style.opacity = 1;
+      //document.querySelector("nav").style.top = "0px";
+    }
+  }
+}
